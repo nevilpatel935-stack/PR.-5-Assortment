0891f1aa35ca5ffc85337d505af84811fc2b1c12
Nevil Patel <nevilpatel935@gmail.com>
2026-06-23 14:55:04 +0530

    first commit

--- prog1.c
+++ prog1.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int j,i;
+    int j, i;
 
     printf("Enter the array size: ");
     scanf("%d", &j);
@@ -10,18 +10,18 @@ int main()
     int a[j];
 
     printf("Enter array elements:\n");
-    for(i=0;i<j;i++)
+    for (i = 0; i < j; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
 
     printf("Negative elements from an Array:");
 
-    for(i=0;i<j;i++)
+    for (i = 0; i < j; i++)
     {
-        if(a[i]<0)
+        if (a[i] < 0)
         {
-            printf("%d ",a[i]);
+            printf("%d ", a[i]);
         }
     }
 
